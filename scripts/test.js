aa130706f28df389212de270b1653f7ce900e1d1
liangxiaojian <271452496@qq.com>
2017-11-15 12:16:46 +0800

    test

---
+++ scripts/test.js
@@ -0,0 +1,15 @@
+"use strict";
+var Student = /** @class */ (function () {
+    function Student(fiestName, lastName) {
+        this.firstName = fiestName;
+        this.lastName = lastName;
+    }
+    Student.prototype.greeter = function () {
+        return "Hello，您好" + this.firstName + " " + this.lastName;
+    };
+    return Student;
+}());
+var user = new Student("王", "小明a ");
+// document.body.innerHTML = user.greeter();
+document.body.innerHTML = user.greeter();
+//# sourceMappingURL=test.js.map
